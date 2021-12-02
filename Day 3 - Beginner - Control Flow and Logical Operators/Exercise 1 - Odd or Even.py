62f6e7ee0b73d3b501849be3d9b19c5ae35f8579
Mahir Sehmi <mnmahir@gmail.com>
2021-12-02 23:39:48 +0800

    Day 3 50%

---
+++ Day 3 - Beginner - Control Flow and Logical Operators/Exercise 1 - Odd or Even.py
@@ -0,0 +1,19 @@
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:   # Greater than or equal to
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
