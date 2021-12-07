ecf2acb9e01e689a2aed438b83674e163211b16d
Mahir Sehmi <mnmahir@gmail.com>
2021-12-08 00:51:39 +0800

    Day 4 Done!

---
+++ Day 4 - Beginner - Randomisation and Python Lists/Exercise 3 - Treasure Map.py
@@ -0,0 +1,27 @@
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+# dirty_dozen = [fruits, vegetables]
+
+# print(dirty_dozen)
+
+# print(dirty_dozen[1][1])
+
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+x = int(position[0])
+y = int(position[1])
+
+map[y-1][x-1] = "X"
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
